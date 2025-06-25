6a7d13eab78bda578517f857c9121df0161d9af7
oldixi <oldixi@yandex.ru>
2025-06-25 09:41:48 +0300

    feat: init commit

---
+++ src/test/java/yandex/practicum/online_shop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package yandex.practicum.online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
